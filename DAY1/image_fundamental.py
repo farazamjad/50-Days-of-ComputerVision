fa6ebf81eb83c56eba66facda5c17aa7d184a486
AsadMinhas78 <faraz.amjad007@gmail.com>
2024-02-21 22:02:48 +0500

    Day1

---
+++ DAY1/image_fundamental.py
@@ -0,0 +1,64 @@
+# !USAGE= python image_fundamental.py --image 'data/task2.jpeg'
+#import libraries
+import argparse
+import cv2
+
+#construct argument parser and parse the arguments
+ap=argparse.ArgumentParser()
+ap.add_argument('-i','--image',type=str,default='data/my.jpg')
+args=vars(ap.parse_args())
+#load the image
+image=cv2.imread(args['image'])
+#extract height and width
+(h,w)=image.shape[:2]
+print(h,w)
+
+#show orignal image to screen
+cv2.imshow("Orignal Image",image)
+#cv2.waitKey(0)
+
+#Images are simply numpy arrays and the origin is (0,0) located at top-left corner
+(b,g,r)=image[0,0]
+#RGB normally but we use BGR as OpenCV uses this format
+
+print(f"Pixel at (0,0) - Red :{r},Green:{g},Blue:{b}")
+
+#x=50 y=20 in matrix format its hxw thats why the cordinates are inverted
+(b,g,r)=image[20,50]
+print(f"Pixel at (50,20) - Red :{r},Green:{g},Blue:{b}")
+
+(b,g,r)=image[330,800]
+print(f"Pixel at (50,20) - Red :{r},Green:{g},Blue:{b}")
+
+#update pixel value at x=50 y=20 and set it to red
+image[20,50]=(0, 0 , 255)
+#             ^  ^     ^
+#             b  g      r
+(b,g,r)=image[20,50]
+print(f"Pixel at (50,20) - Red :{r},Green:{g},Blue:{b}")
+#we can use numpy arrays for cropping and changing pixel values from selected region
+#calculating center coordinates of image
+(cX,cY)=(w//2,h//2)
+print(cX,cY)
+
+#top left corner of image
+#image[startY:endY,startX:endX]
+top_l=image[0:cY,0:cX]
+cv2.imshow("Top Left Corner",top_l)
+
+#top right corner of image
+top_r=image[0:cY,cX:w]
+cv2.imshow("Top Right Corner",top_r)
+
+#bottom left corner of image
+bot_l=image[cY:h,0:cX]
+cv2.imshow("Bottom Left Corner",bot_l)
+#bottom right corner of image
+bot_r=image[cY:h,cX:w]
+cv2.imshow("Bottom Right Corner",bot_r)
+
+#updating top left corner to red
+image[0:cY,cX:w]=top_l
+image[cY:h,cX:w]=bot_l
+cv2.imshow("Updated Top left corner",image)
+cv2.waitKey(0)
---
+++ DAY1/loading_images_with_openCV.py
@@ -0,0 +1,36 @@
+#USAGE
+#   command= python loading_images_with_openCV --input data/my.jpg --output data/myOutput.jpg
+        
+#import libraries
+import argparse 
+import cv2
+
+#argument parsing
+ap=argparse.ArgumentParser()
+ap.add_argument('-i','--input',required=True,help='input path to image')
+ap.add_argument('-o','--output',required=True,help='output path to image')
+
+#converts to dict key:value
+= {INPUT:DATA/MY.JPG}
+args=vars(ap.parse_args())
+
+#loading image using cv2.imread
+
+image=cv2.imread(args['input'])
+#grab spatial dimensions width,height and channels
+(h,w,c)=image.shape[:3]
+
+# 600 pixels x 400 pixels  normally
+#  ^width    ^height   but in (h,w,c) we write like this as in matrix its rows x columns where rows=height and columns = width
+
+print(f"width: {w} pixels")
+print(f"height: {h} pixels")
+print(f"channels: {c}")
+
+# show image and wait for keypress
+cv2.imshow("Jacket sticker",image)
+cv2.waitKey(0)
+
+#write new image, OpenCv handles converting image filetypes automatically)
+cv2.imwrite(args['output'],image)
+
